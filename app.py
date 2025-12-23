e11d3c0be7beb9f6d66618023e47081cecdba715
Fuma <freddz2101@gmail.com>
2025-12-23 22:34:56 +0800

    style changes, added edit & admin page, more

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ class Twoot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(32), nullable=False)
     content = db.Column(db.String(140), nullable=False)
+    is_edited = db.Column(db.Boolean, default=False, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 
@@ -46,12 +47,26 @@ def twoot():
         return render_template("twoot.html", user=session["user"])
 
 
-@app.route('/delete/<int:id>')
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    twoot_to_edit = Twoot.query.get_or_404(id)
+    if request.method == "POST":
+        twoot_to_edit.content = request.form["content"]
+        twoot_to_edit.is_edited = True
+        db.session.commit()
+        return redirect(url_for("index"))
+    else:
+        return render_template("edit.html", twoot=twoot_to_edit)
+
+
+@app.route("/delete/<int:id>")
 def delete(id):
     twoot_to_del = Twoot.query.get_or_404(id)
     try:
         db.session.delete(twoot_to_del)
         db.session.commit()
+        if session["user"] == "fumafuamuf2101":
+            return redirect(url_for("admin"))
         return redirect(url_for("index"))
     except:
         return "Failed to delete"
@@ -64,8 +79,9 @@ def login():
         "xXx_HxNTER K!LLER_xXx",
         "Ladiesman217",
         "Masterbaiter69",
-        "Mr. Dudeman",
-        "Femboy_Slayer_666"
+        "Dudeman",
+        "Femboy_Slayer_666",
+        "Freddy Fazbear"
     ]
     suggestion_nm = random.choice(suggestion_nm_li)
     if request.method == "POST":
@@ -85,9 +101,18 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/secretroom", methods=["GET"])
-def secretroom():
-    pass
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    if "user" in session:
+        if session["user"] == "fumafuamuf2101":
+            twoots = Twoot.query.order_by(Twoot.date_created.desc()).all()
+            return render_template("admin.html", twoots=twoots)
+    return redirect(url_for("index"))
+
+
+@app.route("/secret", methods=["GET"])
+def secret():
+    return render_template("secret.html")
 
 
 if __name__ == "__main__":
