e90054b14b9bd2e0f6e76fc190826ca31f4e997c
Fuma <freddz2101@gmail.com>
2025-12-25 23:52:45 +0800

    image attach

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
+from werkzeug.utils import secure_filename
 import os
+import uuid
 import random
 
 app = Flask(__name__)
+
 app.secret_key = "twoo:3er_uwu_123"
 app.permanent_session_lifetime = timedelta(days=7)
 
@@ -12,15 +15,22 @@ app.permanent_session_lifetime = timedelta(days=7)
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_URL = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+UPLOAD_FOLDER = "static/uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 class Twoot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(32), nullable=False)
     content = db.Column(db.String(320), nullable=False)
+    image = db.Column(db.String(800), nullable=False)
     is_edited = db.Column(db.Boolean, default=False, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -37,13 +47,15 @@ def index():
 
 @app.route("/twoot", methods=["GET", "POST"])
 def twoot():
+    if "user" in session:
+        pass
+    else:
+        return redirect(url_for("login"))
+
     if request.method == "POST":
-        twoot_contents = request.form["content"]
-        new_twoot = Twoot(
-            content=twoot_contents,
-            user=session["user"]
-        )
-        db.session.add(new_twoot)
+        contents = request.form["content"]
+        twoot = Twoot(content=contents, user=session["user"])
+        db.session.add(twoot)
         db.session.commit()
         return redirect(url_for("index"))
     else:
@@ -52,6 +64,11 @@ def twoot():
 
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
 def edit(id):
+    if "user" in session:
+        pass
+    else:
+        return redirect(url_for("login"))
+
     twoot_to_edit = Twoot.query.get_or_404(id)
     if request.method == "POST":
         twoot_to_edit.content = request.form["content"]
@@ -64,8 +81,18 @@ def edit(id):
 
 @app.route("/delete/<int:id>")
 def delete(id):
+    if "user" in session:
+        pass
+    else:
+        return redirect(url_for("login"))
+
     twoot_to_del = Twoot.query.get_or_404(id)
     try:
+        if twoot_to_del.image:
+            try:
+                os.remove(UPLOAD_FOLDER + "/" + twoot_to_del.image)
+            except:
+                pass
         db.session.delete(twoot_to_del)
         db.session.commit()
         if session["user"] == "fumafuamuf2101":
@@ -73,6 +100,29 @@ def delete(id):
         return redirect(url_for("index"))
     except:
         return "Failed to delete"
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/twoot/upload", methods=["POST"])
+def upload():
+    content = request.form["content"]
+    content_file = request.files.get("image")
+
+    filename = None
+    if content_file and content_file.filename != "" and allowed_file(content_file.filename):
+        ext = os.path.splitext(content_file.filename)[1]
+        filename = f"{uuid.uuid4().hex}{ext}"
+        content_file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+    twoot = Twoot(user=session["user"], content=content, image=filename)
+
+    db.session.add(twoot)
+    db.session.commit()
+
+    return redirect(url_for("index"))
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -87,6 +137,7 @@ def login():
         "Freddy Fazbear"
     ]
     suggestion_nm = random.choice(suggestion_nm_li)
+
     if request.method == "POST":
         session.permanent = True
         user = request.form["username"]
@@ -119,6 +170,6 @@ def secret():
 
 
 if __name__ == "__main__":
-    # with app.app_context():
-    # db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
