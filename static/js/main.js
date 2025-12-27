246d1dc40201838af1b7607943033b9ee318db96
Fuma <freddz2101@gmail.com>
2025-12-27 19:08:24 +0800

    js

---
+++ static/js/main.js
@@ -0,0 +1,41 @@
+function showFileName(input) {
+    const fileNameSpan = document.getElementById('fileName');
+    const clearButton = document.getElementById('clearButtonLabel');
+    if (input.files && input.files.length > 0) {
+        fileNameSpan.textContent = input.files[0].name;
+        clearButton.textContent = 'Clear';
+    } else {
+        fileNameSpan.textContent = '';
+        clearButton.textContent = '';
+    }
+}
+
+function clearSelection() {
+    const fileInput = document.getElementById('image');
+    const fileNameSpan = document.getElementById('fileName');
+    const clearButton = document.getElementById('clearButtonLabel');
+    fileInput.value = '';
+    fileNameSpan.textContent = '';
+    clearButton.textContent = '';
+}
+
+function resizeImg(img) {
+    if (img.style.width == '50%') {
+        img.style.width = '100%';
+    } else {
+        img.style.width = '50%';
+    }
+}
+
+function deleteTwoot(id) {
+    fetch(`delete/${id}`, {
+        method: 'DELETE'
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data.success) {
+                const row = document.getElementById(`twoot${id}`)
+                row.remove()
+            }
+        })
+} 
