242b0f7f2fc4e9664ad8fd17369f139b25bb37cd
Fuma <freddz2101@gmail.com>
2026-06-13 21:55:19 +0800

    Changes

--- app.py
+++ app.py
@@ -20,15 +20,13 @@ app = Flask(__name__)
 app.secret_key = "twoo:3er_uwu_123"
 app.permanent_session_lifetime = timedelta(days=7)
 
-# Supabase password: twoo:3ruwu2101
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-# app.config['SQLALCHEMY_DATABASE_URI'] = \
-#   'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
